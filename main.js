6374fad7484871279c68196c779c36cb95887898
Yannieeeeeee <160030575+Yannieeeeeee@users.noreply.github.com>
2024-02-16 19:37:04 -0500

    main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".text", {
+    strings: ["I" , "Love" , "You", "So", "Much"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
+const toTop = document.querySelector(".top");
+window.addEventListener("scroll",() =>{
+    if (window.pageYOffset > 100){
+        toTop.classList.add("active");
